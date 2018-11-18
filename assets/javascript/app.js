3563b84f19f4b1b948c8bde7937f0486e49dce72
jebizar <jebizar@gmail.com>
2018-11-17 20:16:26 -0800

    Updated index.html and app.js with onsen ui and gave search query

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,26 @@
-var queryURL = "https://api.edamam.com/search?q=chicken&app_id=06d1311e&app_key=f04ff3a316728eacadf3124247657efc"
+// ============================================================================================================================
+// Edamam API
+// ============================================================================================================================
+$(document).on("change", '#search', function(){
+    var search = $(this).val();
+    
+    var queryURL = "https://api.edamam.com/search?app_id=06d1311e&app_key=f04ff3a316728eacadf3124247657efc&q=" + search;
 
 $.ajax({
     url: queryURL,
     method: "GET"
 })
-    .then(function(response){
+    .then(function (response) {
         console.log(response);
     });
+})
+
+
+
+// ============================================================================================================================
+// Onsen UI    
+// ============================================================================================================================
+document.addEventListener('prechange', function (event) {
+    document.querySelector('ons-toolbar .center')
+        .innerHTML = event.tabItem.getAttribute('label');
+});
