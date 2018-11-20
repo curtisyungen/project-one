7db30ec50fbda1f52adfeaa7ca5683b1f6c88143
curtisyungen <39065500+curtisyungen@users.noreply.github.com>
2018-11-20 14:39:39 -0800

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,102 @@
+$(document).on("change", "#search", function() {
+  var searchTerm = $(this).val();
+});
+
 // ============================================================================================================================
-// Edamam API
+// Edamam API / Ajax
 // ============================================================================================================================
-$(document).on("change", '#search', function(){
-    var search = $(this).val();
-    
-    // var queryURL = "https://api.edamam.com/search?app_id=06d1311e&app_key=f04ff3a316728eacadf3124247657efc&q=" + search;
 
-    var baseURL = "https://api.edamam.com/search?";
-    var API_KEY = "app_key=f04ff3a316728eacadf3124247657efc";
-    var APP_ID = "app_id=06d1311e";
-    var searchTerm = $(this).val();  
-    var searchLimit = 10;             /* Can be set by user. Default = 10 */
+// var queryURL = "https://api.edamam.com/search?app_id=06d1311e&app_key=f04ff3a316728eacadf3124247657efc&q=" + searchTerm
 
-    // Set up recipe object
-    var recipe = {
-        name: "default",
-        rating: 5,
-        ingredients: "",
-        difficulty: 5,
-    }
+var baseURL = "https://api.edamam.com/search?";
+var API_KEY = "f04ff3a316728eacadf3124247657efc";
+var APP_ID = "06d1311e";
+var searchTerm = "green bean casserole";
+var searchLimit = 1;             // can be set by user
 
-  var queryURL = `${baseURL}${APP_ID}&${API_KEY}&q=${searchTerm}`;
+var queryURL = `${baseURL}app_id=${APP_ID}&app_key=${API_KEY}&q=${searchTerm}`;
+
+// ================
+// Recipe Object
+// ================
+
+var recipe = {
+    name: "default",
+    // rating: 5,
+    ingredients: "",
+    dietLabels: "",
+    image: "",
+    // difficulty: 5,
+    url: "",
+}
+
+// ================
+// Ajax Query
+// ================
 
 $.ajax({
     url: queryURL,
     method: "GET"
-})
-    .then(function (response) {
-        for (var i=0; i<searchLimit; i++) {
+}).then(function(response) {
 
-            console.log(response.hits[i].recipe.label);
-    
-        }    ;
-    });
-})
+    // Extract recipes from Ajax response
 
+    for (var i=0; i<searchLimit; i++) {
 
+        // Create div for each recipe
+
+        var resultDiv = $("<div>");
+
+        // Give recipe a class
+
+        resultDiv.addClass("recipeDiv");
+
+        // Give an id to each recipe
+
+        resultDiv.attr("id", i);
+
+        // Add data to recipe object
+
+        recipe.name = response.hits[i].recipe.label;
+        recipe.ingredients = response.hits[i].recipe.ingredientLines;
+        //recipe.dietLabels = response.hits[i].recipe.dietLabels;
+        //recipe.image = response.hits[i].recipe.image;
+        //recipe.url = response.hits[i].recipe.url;
+
+        // Add object info to div for display
+
+        resultDiv.html(
+            `${recipe.name} <br>
+             ${recipe.ingredients} <br>
+             ${recipe.image} <br>
+             ${recipe.url} <br>`
+            );
+
+        // Create image to append to testDiv
+
+        // var testImage = $("<img>");
+        // testImage.attr("src", recipe.image);
+        // resultDiv.prepend(testImage);
+        
+        $(".page__content").append(recipeDiv);
+
+	// Console log the object data
+
+        console.log(response.hits[i].recipe);
+    }
+});
+
+$(document).on("click", ".recipeDiv", openRecipe);
+
+function openRecipe() {
+  console.log("yes");
+}
 
 // ============================================================================================================================
 // Onsen UI    
 // ============================================================================================================================
+
 document.addEventListener('prechange', function (event) {
     document.querySelector('ons-toolbar .center')
         .innerHTML = event.tabItem.getAttribute('label');
-});
+}); 
