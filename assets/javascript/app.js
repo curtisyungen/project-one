fe8b624da00224a36c746897de0dd4165aab0b1d
Carson Wack <carson_wack@yahoo.com>
2018-11-25 12:59:18 -0800

    added form for searching google API for clipart image -- populates page with image on add click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,29 @@
 var queryURL = "https://api.edamam.com/search?q=chicken&app_id=06d1311e&app_key=f04ff3a316728eacadf3124247657efc"
 
 $.ajax({
-    url: queryURL,
-    method: "GET"
+  url: queryURL,
+  method: "GET"
 })
-    .then(function(response){
-        console.log(response);
+  .then(function (response) {
+    console.log(response);
+  });
+
+
+
+
+$(document).on('click', '#add-item', function () {
+  event.preventDefault();
+  let userInput = $('#food-input').val().trim();
+  let URL = `https://www.googleapis.com/customsearch/v1?q=${userInput}&cx=003819080641655921957%3A-osseiuyk9e&imgType=clipart&num=1&searchType=image&key=AIzaSyDJ90SaiND0l5GJlYS-rAnWNcWFZIoDNL8`;
+
+  $.ajax({
+    url: URL,
+    method: 'GET'
+  })
+    .then(function (response) {
+
+      let thumbnail = $('<img>');
+      thumbnail.attr('src', response.items[0].image.thumbnailLink);
+      $('#google-api-image').append(thumbnail);
     });
+});
